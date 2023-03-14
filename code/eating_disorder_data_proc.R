a198e8e6772c8a74a111e664fc3545dab16fd40c
Simeon Platte <platte@mailbox.org>
2023-03-14 17:53:05 +0100

    Add combined covid_pan+lockdown variables for Eating disorder dataset.

--- code/eating_disorder_data_proc.R
+++ code/eating_disorder_data_proc.R
@@ -19,18 +19,30 @@ df_diag <- data_exp$diagnosis %>%
          f50_type = factor(if_else(icd_cat_l1 == "Essstörung", icd_cat_l3, "F50-"),
                            levels = c("F50-", "Essstörung Sonst.", "Bulimie", "Anorexie"), ordered = TRUE))
 
-df_ed <- data_exp$case %>%
+df_ed <-
+  # case
+  data_exp$case %>%
   select(-source_dfs) %>%
   filter(between(adm_date, study_start, study_end)) %>%
+  # patient
   left_join(data_exp$patient %>% select(-source_dfs), by = "p_id") %>%
+  # diagnosis
   inner_join(df_diag %>%
                summarise(icd_f50 = max(icd_f50, na.rm = TRUE),
                          f50_type = max(f50_type, na.rm = TRUE),
                          .by = case_id),
              by = "case_id") %>%
-  inner_join(data_exp_sum$diagnosis %>% select(-case_id_orig), by = "case_id")
+  inner_join(data_exp_sum$diagnosis %>% select(-case_id_orig), by = "case_id") %>%
+  # unify covid_pan+lockdown
+  mutate(covid_lockd = if_else(covid_pan == "pre_covid", as.character(covid_pan), as.character(lockdown_status)) %>%
+           replace_na("0") %>%
+           ordered(levels = c("pre_covid", "0", "1")) %>%
+           fct_recode(covid_open = "0", lockd = "1"),
+         covid_lockd_n = if_else(covid_pan == "pre_covid", as.character(covid_pan), as.character(lockdown_period_n)) %>%
+           replace_na("0") %>%
+           ordered(levels = c("pre_covid", "0", "1", "2")) %>%
+           fct_recode(covid_open = "0", lockd_1 = "1", lockd_2 = "2"))
 
 pre_cov_dur <- min(df_ed$adm_date) %--% covid_start %>% as.numeric("years")
 cov_dur <- covid_start %--% max(df_ed$adm_date) %>% as.numeric("years")
 
-if(do_save_objects) saveRDS(df_ed, str_glue("output/CoverCHILD_data_{Sys.Date()}.rds"))
