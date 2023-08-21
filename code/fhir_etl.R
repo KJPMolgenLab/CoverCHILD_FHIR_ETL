50bcaa1b3f57daf194d9a1650e0c14ecf5eb9ac4
Simeon Platte <platte@mailbox.org>
2023-08-21 15:45:41 +0100

    Add timing functionality to FHIR ETL.

--- code/fhir_etl.R
+++ code/fhir_etl.R
@@ -8,10 +8,12 @@
 ## load & install ----
 source("code/functions.R")
 inst_pkgs("config", "httr")
-load_inst_pkgs("tidyverse", "magrittr", "fhircrackr", "data.table")
+load_inst_pkgs("tidyverse", "magrittr", "fhircrackr", "data.table", "tictoc")
 
 cfg <- config::get(file = file.path("config", "fhir_cfg.yml"))
 search_cfg <- config::get(file = file.path("config", "fhir_search_cfg.yml"))
+
+tlog_path <- file.path("output", paste0("FHIR_timings_", format(Sys.time(), "%y%m%d-%H%M"), ".csv"))
 
 ## network settings ----
 # proxy
@@ -25,15 +27,15 @@ if (!cfg$ssl_verify_peer) httr::set_config(httr::config(ssl_verifypeer = 0L))
 # - add config options for parallelization
 # - Where will these scripts be executed? Single node only?
 
-#TODO: Can resource elements already be limited to search_cfg$elements in fhir_search_urls via _elements param?
-
 ## check config ----
 #TODO: protect config yml against type errors & provide default fallback values.
 #      e.g. rectify bools with easyr::tobool
 
 
+ctic("Run FHIR ETL.")
 # retrieve & crack bundles  --------------------------------------------------------------------------------------------
 ## retrieve FHIR bundles (load or search) ----
+ctic("Retrieve FHIR bundles (load or search).")
 if (cfg$do_search) {
   # separate config entries so fhir_url() can check the elements
   search_cfg$named_params <-
@@ -62,6 +64,7 @@ if (cfg$do_search) {
 } else {
   fhir_bundles <- fhir_load(cfg$filebase, pattern = "\\.xml$")
 }
+ctoc_log(save = tlog_path)
 gc()
 gc()
 
@@ -72,18 +75,21 @@ fhir_elements <- fhir_design(imap(
   \(elements, resource_name) fhir_table_description(resource = str_to_title(resource_name),
                                                     cols = if (!cfg$keep_all_elements) elements)))
 
+ctic("Crack FHIR bundles.")
 fhir_dfs <- fhir_crack(bundles = fhir_bundles,
                        design = fhir_elements,
                        sep = cfg$sep,
                        brackets = cfg$brackets,
                        rm_empty_cols = FALSE,
                        ncores = cfg$n_cores)
+ctoc_log(save = tlog_path)
 gc()
 gc()
 
 # melt
 #TODO: This needs debugging/expansion, if multiple melt-groups per DF are present
 #      or when grouping attribute is not on level 1.
+ctic("Melt FHIR DFs.")
 molten_dfs <- imap(fhir_dfs, \(df, df_name) {
   melt_groups <- find_melt_col_groups(df, cfg$brackets)
   if (length(melt_groups) > 1) warning(str_glue("DF '{df_name}' has more than 1 melt group: ",
@@ -100,8 +106,11 @@ molten_dfs <- imap(fhir_dfs, \(df, df_name) {
     }
   fhir_rm_indices(df, brackets = cfg$brackets)
   })
+ctoc_log(save = tlog_path)
 gc()
 gc()
 
 # bring dfs to global env for easier inspection
 for (x in names(molten_dfs)) assign(paste0("df_", x), fhir_dfs[[x]])
+
+ctoc_log(save = tlog_path)
