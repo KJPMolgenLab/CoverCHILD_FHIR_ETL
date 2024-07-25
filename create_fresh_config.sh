9d393001f415cb7f3e9a837c6e7beda4a3460752
Gierschner, Christian <christian.gierschner@ukdd.de>
2024-07-25 15:01:57 +0200

    add custom coverCHILD config file

--- create_fresh_config.sh
+++ create_fresh_config.sh
@@ -2,7 +2,7 @@
 
 CFG_PATH="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/config"
 
-for CFG_FILE in "fhir_cfg" "fhir_search_cfg"; do
+for CFG_FILE in "fhir_cfg" "fhir_search_cfg" "coverchild_cfg"; do
   cp "$CFG_PATH/EXAMPLE_${CFG_FILE}.yml" "$CFG_PATH/${CFG_FILE}.yml"
 done
 
